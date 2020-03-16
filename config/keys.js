def0603bb8d7305d7f1b897320c3a9164489c771
jordantom <jordan.tom88@gmail.com>
2020-03-16 09:41:10 -0700

    remove key from keys file

--- config/keys.js
+++ config/keys.js
@@ -5,7 +5,3 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_dev');
 }
 
-// module.exports = {
-//     mongoURI: 'mongodb+srv://jtom2388:StephKlayDurant2018!@cluster0-bgmqm.mongodb.net/test?retryWrites=true&w=majority',
-//     secretOrKey: 'secret'
-// }
